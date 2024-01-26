af20e9095db418bef346e5c5aa10b7bb8a9c015c
OzanEFETopcu <ozanefe.topcu@gmail.com>
2024-01-26 13:00:07 +0200

    basic/011: add Javadoc to main

---
+++ basic/011/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * @author      Ozan Topcu <ozan.topcu@tuni.fi>
+ * @version     2024.0126 (last modified)
+ * @since       21.0 (miminimum Java version)
+ */
+class HelloWorld {
+
+    /**
+     * Exercise: 1.1
+     * Write the Hello World program.
+     *
+     * @param  args  Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// End of file
