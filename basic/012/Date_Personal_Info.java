0f3043b743c8bdf26983d2430682141d095cfce1
OzanEFETopcu <ozanefe.topcu@gmail.com>
2024-01-29 18:55:53 +0200

    basic/012: Creating the setPersonal info and printing the final message

--- basic/012/Date_Personal_Info.java
+++ basic/012/Date_Personal_Info.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * @author Ozan Topcu <ozan.topcu@tuni.fi>
@@ -12,11 +13,20 @@ public class Date_Personal_Info {
      *
      * @param args Command line arguments. Not used.
      */
-    public static Date getDate() {
-        return 
+    public static String getDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD");
+        Date date = new Date();
+        return "The date is " + formatter.format(date) + "\n";
+    }
+
+    public static String setPersonalInfo(String first, String last, String height) {
+
+        return "Hello, I'm "+first+" "+last+"\nI'm "+height+" cm tall";
     }
 
     public static void main(String[] args) {
-
+        String currentDate = getDate();
+        String personalInfo = setPersonalInfo("Ozan", "Topcu", "176");
+        System.out.println(currentDate + personalInfo);
     }
 }
