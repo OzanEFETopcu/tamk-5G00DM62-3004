c8177fe307190eee3fdc421010b71f458a648a35
OzanEFETopcu <ozanefe.topcu@gmail.com>
2024-01-29 18:38:29 +0200

    basic/012: Creating getDate function

---
+++ basic/012/Date_Personal_Info.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+/**
+ * @author Ozan Topcu <ozan.topcu@tuni.fi>
+ * @version 2024.0129
+ * @since 17.0
+ */
+public class Date_Personal_Info {
+    /**
+     * Exercise: 1.2
+     * Write the Date program.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static Date getDate() {
+        return 
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
